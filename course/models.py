599011e836979e2aabfe6ae024090c6023a7c368
beimenchuixue <jia2jiayuan@163.com>
2018-02-06 13:03:41 +0800

    新建django course app，这个app用于处理课程相关的信息，在这个app中新增 课程表、章节表、视频信息表、课程资源表

---
+++ course/models.py
@@ -0,0 +1,94 @@
+from datetime import datetime
+
+from django.db import models
+
+# Create your models here.
+
+
+class Course(models.Model):
+    """
+    定义课程表，包含课程名、课程封面图、课程简介、课程详情（为富文本编辑框）、课程时长（分钟）
+    定义记录统计信息： 学习该课程人数、课程收藏数、课程点击量
+    """
+    # 课程难度等级
+    course_degree = (
+        ('cj', '初级'),
+        ('zj', '中级'),
+        ('gj', '高级')
+    )
+    name = models.CharField(max_length=50, verbose_name='课程名')
+    course_image = models.ImageField(upload_to='course/%Y/%m', verbose_name='课程封面图')
+    desc = models.CharField(max_length=300, verbose_name='课程简介')
+    detail = models.TextField(verbose_name='课程详情')
+    degree = models.CharField(max_length=2, choices=course_degree, verbose_name='课程难度')
+    learn_time = models.IntegerField(default=0, verbose_name='学习总时长(分钟)')
+
+    # 统计
+    students = models.IntegerField(default=0, verbose_name='学习人数')
+    fav_num = models.IntegerField(default=0, verbose_name='收藏人数')
+    click_num = models.IntegerField(default=0, verbose_name='课程点击量')
+
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '课程'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    """
+    章节表，只包含章节名、添加时间、一个关联课程表的外键
+    """
+    # 章节与课程是多对一关系，需要在多关系的一方建立一个外键关联少的一方主键
+    # 关联外键，通过django的 ForeignKey 只需要注明想要关联的表
+    # 会自动获取关联表的ID并创建关联字段
+    course = models.ForeignKey(Course, verbose_name='课程')
+
+    name = models.CharField(max_length=100, verbose_name='章节名')
+
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '章节'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class Video(models.Model):
+    """
+    定义章节下视频信息。包含关联Lesson表的外键、视频名字、添加时间
+    """
+    lesson = models.ForeignKey(Lesson, verbose_name='章节')
+    name = models.CharField(max_length=100, verbose_name='视频名')
+
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '章节视频'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class CourseResource(models.Model):
+    """
+    课程资源信息，包含 关联课程外键、资源名称、下载资源地址、添加时间信息
+    """
+    course = models.ForeignKey(Course, verbose_name='课程')
+    name = models.CharField(max_length=100, verbose_name='资源名称')
+    # 需要从后台上传资源到指定目录
+    download = models.FileField(upload_to='course/resource/%Y/%m', max_length=100, verbose_name='资源地址')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '课程资源'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
